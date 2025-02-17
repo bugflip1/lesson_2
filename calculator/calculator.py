dd14def0178a3253e72d615e63f6c75a124f5984
Jonathan Felix <jfelix@Jonathans-MacBook-Pro-2.local>
2025-02-16 19:43:52 -1000

    forgot to branch, worked on language selection

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -9,18 +9,52 @@ with open('calculator/calculator.json', 'r') as file:
 
 # FUNCTIONS #
 
+def welcome(): # Welcome message
+    print(prompt(data["english"]["welcome_messages"]))
+
+def prompt(message): # Adds a arrow to the start of messages
+    return f"==> {message}"      
+
+def lanassign(): # Assigns language grabber value to a variable and maps it data
+    return data[languagegrabber()]
+
+def languagegrabber(): # Language grabber
+    lan = input(prompt(data["english"]["langrabber"])).lower()
+    return lan
+
+def main(): # Main function
+
+    # Ask the user for the first number
+    number1 = input(prompt(lan["numoneprompt"]))
+    while not is_numeric(number1):
+        number1 = input(prompt(lan["invalidnum"]))
+
+    # Ask the user for the second number
+    number2 = input(prompt(lan["numtwoprompt"]))
+    while not is_numeric(number2):
+        number2 = input(prompt(lan["invalidnum"]))
+
+
+    # Ask the user for the operation to perform
+    operation = input(prompt(lan["opprompt"]))
+
+    # Perform the operation on the two numbers
+    number1, number2 = conv_int(number1, number2)
+
+    # Print the result to the terminal
+    print(
+        f'{lan["result"]} {operation_complete(number1, number2, operation)}'
+        )
+
 def is_numeric(s): # Is numeric function, really only checks for floats
     try:
         float(s)
         return True
     except ValueError:
         return False
-
+    
 def conv_int(num1, num2): # Converts the input to floats
     return float(num1), float(num2)
-
-def prompt(message): # Adds a arrow to the start of messages
-    return f"==> {message}"
 
 def operation_complete(num1, num2, op): # pylint: disable=unused-argument
     match op:
@@ -37,39 +71,12 @@ def operation_complete(num1, num2, op): # pylint: disable=unused-argument
 
         case _:
             user_operation = input(prompt(
-                data["invalidop"]
+                lan["invalidop"]
                 ))
             return operation_complete(num1, num2, user_operation)
-
-def welcome(): # Welcome message
-    print(data["welcome_messages"])
-
-def main(): # Main function
-
-    # Ask the user for the first number
-    number1 = input(prompt(data["numoneprompt"]))
-    while not is_numeric(number1):
-        number1 = input(prompt(data["invalidnum"]))
-
-    # Ask the user for the second number
-    number2 = input(prompt(data["numtwoprompt"]))
-    while not is_numeric(number2):
-        number2 = input(prompt(data["invalidnum"]))
-
-
-    # Ask the user for the operation to perform
-    operation = input(prompt(data["opprompt"]))
-
-    # Perform the operation on the two numbers
-    number1, number2 = conv_int(number1, number2)
-
-    # Print the result to the terminal
-    print(
-        f'{data["result"]} {operation_complete(number1, number2, operation)}'
-        )
-
+        
 def rerun(): # Asks if the user wants to do another operation, loops itself
-    user_input = input(prompt(data["rerun"]))
+    user_input = input(prompt(lan["rerun"]))
     if user_input.lower() == 'y':
         main()
         rerun()
@@ -81,5 +88,6 @@ def rerun(): # Asks if the user wants to do another operation, loops itself
 
 # MAIN #
 welcome()
+lan = lanassign()
 main()
 rerun()
