7e7b8c144858486937ab83fd4a2370387df75ac4
Jonathan Felix <jfelix@Jonathans-MacBook-Pro-2.local>
2025-02-16 17:58:48 -1000

    added option for user to rerun the program

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,21 +1,16 @@
-# Is numeric function, really checks for float.
-def is_numeric(s):
+# FUNCTIONS #
+
+def is_numeric(s): # Is numeric function, really only checks for floats
     try:
         float(s)
         return True
     except ValueError:
         return False
-
-    try:
-        int(s)
-        return True
-    except ValueError:
-        return False
-
-def conv_int(num1, num2):
+    
+def conv_int(num1, num2): # Converts the input to floats
     return float(num1), float(num2)
 
-def prompt(message):
+def prompt(message): # Adds a arrow to the start of messages
     return f"==> {message}"
 
 def operation_complete(num1, num2, op): # pylint: disable=unused-argument
@@ -38,29 +33,47 @@ def operation_complete(num1, num2, op): # pylint: disable=unused-argument
                 ))
             return operation_complete(num1, num2, user_operation)
 
-# Welcome
-print('Welcome to Calculator!')
+def welcome(): # Welcome message
+    print('Welcome to Calculator!')
 
-# Ask the user for the first number
-number1 = input(prompt("What's the first number?\n"))
-while not is_numeric(number1):
-    number1 = input(prompt("Please enter a valid number!\n"))
+def main(): # Main function
 
-# Ask the user for the second number
-number2 = input(prompt("What's the second number?\n"))
-while not is_numeric(number2):
-    number1 = input(prompt("Please enter a valid number!\n"))
+    # Ask the user for the first number
+    number1 = input(prompt("What's the first number?\n"))
+    while not is_numeric(number1):
+        number1 = input(prompt("Please enter a valid number!\n"))
+
+    # Ask the user for the second number
+    number2 = input(prompt("What's the second number?\n"))
+    while not is_numeric(number2):
+        number1 = input(prompt("Please enter a valid number!\n"))
 
 
-# Ask the user for the operation to perform
-operation = input(prompt(
-    "What operation would you like to perform?\n"
-    '"+" for addition\n"-" for subtraction\n"*" for multiplication\n'
-    '"/" for division\nEnter here: '
-    ))
+    # Ask the user for the operation to perform
+    operation = input(prompt(
+        "What operation would you like to perform?\n"
+        '"+" for addition\n"-" for subtraction\n"*" for multiplication\n'
+        '"/" for division\nEnter here: '
+        ))
 
-# Perform the operation on the two numbers
-number1, number2 = conv_int(number1, number2)
+    # Perform the operation on the two numbers
+    number1, number2 = conv_int(number1, number2)
 
-# Print the result to the terminal
-print(f"The result is: {operation_complete(number1, number2, operation)}")
+    # Print the result to the terminal
+    print(f"The result is: {operation_complete(number1, number2, operation)}")
+
+def rerun(): # Asks if the user wants to do another operation, loops itself
+    user_input = input(prompt("Would you like to perform another operation?\n Enter Y or N: "))
+    if user_input.lower() == 'y':
+        main()
+        rerun()
+    elif user_input.lower() == 'n':
+        print("Goodbye!")
+
+# FUNCTIONS END #'
+
+
+# MAIN #
+welcome()13245
+main()
+rerun()
