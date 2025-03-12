1887fed3e3b8ae7692fdc4c422e9c7a31b7a72e7
Jonathan Felix <jmfelix2003@gmail.com>
2025-03-11 22:04:45 -1000

    update question four in hard one

--- lesson_three/practice_problems_hard_one/question_four.py
+++ lesson_three/practice_problems_hard_one/question_four.py
@@ -1,5 +1,5 @@
 def is_an_ip_number(this_is_a_filler_so_the_file_isnt_red_in_vscode):
-    return False
+    return this_is_a_filler_so_the_file_isnt_red_in_vscode
 
 def is_dot_separated_ip_address(input_string):
     dot_separated_words = input_string.split(".")
